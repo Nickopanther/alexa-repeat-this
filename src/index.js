b26b6a9b467871b8b8f902d5ece23eb5c8769eff
Greg Yeutter <greg@Gregs-MacBook-Pro.local>
2016-11-20 19:41:10 -0500

    Finish implementing skill basic function

--- src/index.js
+++ src/index.js
@@ -107,10 +107,12 @@ RepeatThisSkill.prototype.intentHandlers = {
 
 function getWelcomeResponse(response) {
     // If we wanted to initialize the session to have some attributes we could add those here.
-    var cardTitle = "This Day in History";
-    var repromptText = "With History Buff, you can get historical events for any day of the year. For example, you could say today, or August thirtieth. Now, which day do you want?";
-    var speechText = "<p>History buff.</p> <p>What day do you want events for?</p>";
-    var cardOutput = "History Buff. What day do you want events for?";
+    var cardTitle = "Repeat This";
+    var repromptText = "Repeat This repeats a short phrase you specify.  " +
+            "For example, you could say the meaning of life is 42 and I would say the meaning of life is 42. " + 
+            "Now, what would you like me to repeat?";
+    var speechText = "<p>Repeat This.</p> <p>What would you like me to say?</p>";
+    var cardOutput = "Repeat This. What would you like me to say?";
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
 
@@ -129,150 +131,70 @@ function getWelcomeResponse(response) {
  * Gets a poster prepares the speech to reply to the user.
  */
 function handleFirstEventRequest(intent, session, response) {
-    var daySlot = intent.slots.day;
-    var repromptText = "With History Buff, you can get historical events for any day of the year. For example, you could say today, or August thirtieth. Now, which day do you want?";
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-                      "July", "August", "September", "October", "November", "December"
-    ];
+    var repromptText = "Repeat This repeats a short phrase you specify.  " +
+            "For example, you could say the meaning of life is 42 and I would say the meaning of life is 42. " + 
+            "Now, what would you like me to repeat?"
+
+    var input = intent.slots.input;
+    
     var sessionAttributes = {};
-    // Read the first 3 events, then set the count to 3
-    sessionAttributes.index = paginationSize;
-    var date = "";
 
-    // If the user provides a date, then use that, otherwise use today
-    // The date is in server time, not in the user's time zone. So "today" for the user may actually be tomorrow
-    if (daySlot && daySlot.value) {
-        date = new Date(daySlot.value);
-    } else {
-        date = new Date();
-    }
+    var prefixContent = "<p>Your phrase: </p>";
+    var cardContent = "Your phrase: ";
 
-    var prefixContent = "<p>For " + monthNames[date.getMonth()] + " " + date.getDate() + ", </p>";
-    var cardContent = "For " + monthNames[date.getMonth()] + " " + date.getDate() + ", ";
+    var cardTitle = "Repeat This";
 
-    var cardTitle = "Events on " + monthNames[date.getMonth()] + " " + date.getDate();
+    sessionAttributes.text = input;
+    session.attributes = sessionAttributes;
 
-    getJsonEventsFromWikipedia(monthNames[date.getMonth()], date.getDate(), function (events) {
-        var speechText = "",
-            i;
-        sessionAttributes.text = events;
-        session.attributes = sessionAttributes;
-        if (events.length == 0) {
-            speechText = "There is a problem connecting to Wikipedia at this time. Please try again later.";
-            cardContent = speechText;
-            response.tell(speechText);
-        } else {
-            for (i = 0; i < paginationSize; i++) {
-                cardContent = cardContent + events[i] + " ";
-                speechText = "<p>" + speechText + events[i] + "</p> ";
-            }
-            speechText = speechText + "<p>Wanna go deeper in history?</p>";
-            var speechOutput = {
-                speech: "<speak>" + prefixContent + speechText + "</speak>",
-                type: AlexaSkill.speechOutputType.SSML
-            };
-            var repromptOutput = {
-                speech: repromptText,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
-        }
-    });
+    speechText = input;
+    cardContent = speechText;
+
+    response.tell(speechText);
+    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
 }
 
 /**
  * Gets a poster prepares the speech to reply to the user.
  */
-function handleNextEventRequest(intent, session, response) {
-    var cardTitle = "More events on this day in history",
-        sessionAttributes = session.attributes,
-        result = sessionAttributes.text,
-        speechText = "",
-        cardContent = "",
-        repromptText = "Do you want to know more about what happened on this date?",
-        i;
-    if (!result) {
-        speechText = "With History Buff, you can get historical events for any day of the year. For example, you could say today, or August thirtieth. Now, which day do you want?";
-        cardContent = speechText;
-    } else if (sessionAttributes.index >= result.length) {
-        speechText = "There are no more events for this date. Try another date by saying <break time = \"0.3s\"/> get events for august thirtieth.";
-        cardContent = "There are no more events for this date. Try another date by saying, get events for august thirtieth.";
-    } else {
-        for (i = 0; i < paginationSize; i++) {
-            if (sessionAttributes.index>= result.length) {
-                break;
-            }
-            speechText = speechText + "<p>" + result[sessionAttributes.index] + "</p> ";
-            cardContent = cardContent + result[sessionAttributes.index] + " ";
-            sessionAttributes.index++;
-        }
-        if (sessionAttributes.index < result.length) {
-            speechText = speechText + " Wanna go deeper in history?";
-            cardContent = cardContent + " Wanna go deeper in history?";
-        }
-    }
-    var speechOutput = {
-        speech: "<speak>" + speechText + "</speak>",
-        type: AlexaSkill.speechOutputType.SSML
-    };
-    var repromptOutput = {
-        speech: repromptText,
-        type: AlexaSkill.speechOutputType.PLAIN_TEXT
-    };
-    response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
-}
-
-function getJsonEventsFromWikipedia(day, date, eventCallback) {
-    var url = urlPrefix + day + '_' + date;
-
-    https.get(url, function(res) {
-        var body = '';
-
-        res.on('data', function (chunk) {
-            body += chunk;
-        });
-
-        res.on('end', function () {
-            var stringResult = parseJson(body);
-            eventCallback(stringResult);
-        });
-    }).on('error', function (e) {
-        console.log("Got error: ", e);
-    });
-}
-
-function parseJson(inputText) {
-    // sizeOf (/nEvents/n) is 10
-    var text = inputText.substring(inputText.indexOf("\\nEvents\\n")+10, inputText.indexOf("\\n\\n\\nBirths")),
-        retArr = [],
-        retString = "",
-        endIndex,
-        startIndex = 0;
-
-    if (text.length == 0) {
-        return retArr;
-    }
-
-    while(true) {
-        endIndex = text.indexOf("\\n", startIndex+delimiterSize);
-        var eventText = (endIndex == -1 ? text.substring(startIndex) : text.substring(startIndex, endIndex));
-        // replace dashes returned in text from Wikipedia's API
-        eventText = eventText.replace(/\\u2013\s*/g, '');
-        // add comma after year so Alexa pauses before continuing with the sentence
-        eventText = eventText.replace(/(^\d+)/,'$1,');
-        eventText = 'In ' + eventText;
-        startIndex = endIndex+delimiterSize;
-        retArr.push(eventText);
-        if (endIndex == -1) {
-            break;
-        }
-    }
-    if (retString != "") {
-        retArr.push(retString);
-    }
-    retArr.reverse();
-    return retArr;
-}
+// function handleNextEventRequest(intent, session, response) {
+//     var cardTitle = "More events on this day in history",
+//         sessionAttributes = session.attributes,
+//         result = sessionAttributes.text,
+//         speechText = "",
+//         cardContent = "",
+//         repromptText = "Do you want to know more about what happened on this date?",
+//         i;
+//     if (!result) {
+//         speechText = "With History Buff, you can get historical events for any day of the year. For example, you could say today, or August thirtieth. Now, which day do you want?";
+//         cardContent = speechText;
+//     } else if (sessionAttributes.index >= result.length) {
+//         speechText = "There are no more events for this date. Try another date by saying <break time = \"0.3s\"/> get events for august thirtieth.";
+//         cardContent = "There are no more events for this date. Try another date by saying, get events for august thirtieth.";
+//     } else {
+//         for (i = 0; i < paginationSize; i++) {
+//             if (sessionAttributes.index>= result.length) {
+//                 break;
+//             }
+//             speechText = speechText + "<p>" + result[sessionAttributes.index] + "</p> ";
+//             cardContent = cardContent + result[sessionAttributes.index] + " ";
+//             sessionAttributes.index++;
+//         }
+//         if (sessionAttributes.index < result.length) {
+//             speechText = speechText + " Wanna go deeper in history?";
+//             cardContent = cardContent + " Wanna go deeper in history?";
+//         }
+//     }
+//     var speechOutput = {
+//         speech: "<speak>" + speechText + "</speak>",
+//         type: AlexaSkill.speechOutputType.SSML
+//     };
+//     var repromptOutput = {
+//         speech: repromptText,
+//         type: AlexaSkill.speechOutputType.PLAIN_TEXT
+//     };
+//     response.askWithCard(speechOutput, repromptOutput, cardTitle, cardContent);
+// }
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
